0a247234d45831ca0931c5dc7f21a99539a53f77
Mark Evans <mark@new-bamboo.co.uk>
2013-05-03 11:28:50 +0100

    initial commit: rotating cube

---
+++ js/setup.js
@@ -0,0 +1,22 @@
+(function () {
+  window.setup = function (onSetup, onDraw) {
+    var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 10000 )
+    var scene = new THREE.Scene()
+    var renderer = new THREE.WebGLRenderer()
+
+    renderer.setSize( window.innerWidth, window.innerHeight )
+    document.body.appendChild( renderer.domElement )
+
+    function animate() {
+      requestAnimationFrame( animate )
+      onDraw(scene, camera)
+      renderer.render( scene, camera )
+    }
+
+    // For the console
+    window.camera = camera
+
+    onSetup(scene, camera)
+    animate()
+  }
+})()
